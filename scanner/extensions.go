6633703584f2e146d3e6acbea99bd77d9661238a
edoardottt <edoardott@gmail.com>
2021-04-30 19:32:22 +0200

    update

--- scanner/extensions.go
+++ scanner/extensions.go
@@ -1,47 +1,55 @@
 package scanner
 
-func GetExtensions() map[string]int {
+//FileType
+type FileType struct {
+	extension string
+	severity  int
+	alert     bool
+}
+
+func GetExtensions() []FileType {
 	//extensions contains a list of known extensions
 	//and the TYPICAL (also say `in general`) associated severity.
-	var extensions = map[string]int{
-		"key":        1,
-		"env":        1,
-		"pem":        1,
-		"git":        1,
-		"ovpn":       1,
-		"log":        1,
-		"secret":     1,
-		"secrets":    1,
-		"bak":        1,
-		"dat":        1,
-		"db":         1,
-		"sh":         2,
-		"py":         2,
-		"json":       2,
-		"xml":        2,
-		"yml":        2,
-		"yaml":       2,
-		"properties": 2,
-		"toml":       2,
-		"dtd":        2,
-		"php4":       2,
-		"zip":        3,
-		"doc":        3,
-		"docx":       3,
-		"dochtml":    3,
-		"csv":        3,
-		"odt":        3,
-		"xls":        3,
-		"xlsx":       3,
-		"txt":        3,
-		"ts":         4,
-		"js":         4,
-		"php":        5,
-		"phtml":      5,
-		"php5":       5,
-		"html":       6,
-		"htm":        6,
-		"pdf":        7,
+	var extensions = []FileType{
+		{"key", 1, true},
+		{"env", 1, true},
+		{"pem", 1, true},
+		{"git", 1, true},
+		{"ovpn", 1, true},
+		{"log", 1, true},
+		{"secret", 1, true},
+		{"secrets", 1, true},
+		{"bak", 1, true},
+		{"dat", 1, true},
+		{"db", 1, true},
+		{"sh", 2, true},
+		{"py", 2, true},
+		{"json", 2, true},
+		{"xml", 2, true},
+		{"yml", 2, true},
+		{"yaml", 2, true},
+		{"properties", 2, true},
+		{"toml", 2, true},
+		{"dtd", 2, true},
+		{"php4", 2, true},
+		{"conf", 2, true},
+		{"zip", 3, true},
+		{"doc", 3, false},
+		{"docx", 3, false},
+		{"dochtml", 3, false},
+		{"csv", 3, false},
+		{"odt", 3, false},
+		{"xls", 3, false},
+		{"xlsx", 3, false},
+		{"txt", 3, false},
+		{"ts", 4, false},
+		{"js", 4, false},
+		{"php", 5, false},
+		{"phtml", 5, false},
+		{"php5", 5, false},
+		{"html", 6, false},
+		{"htm", 6, false},
+		{"pdf", 7, false},
 	}
 
 	return extensions
