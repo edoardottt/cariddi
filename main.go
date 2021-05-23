ced02d5deef1865ee48ae14ffb082933c258fdd2
edoardottt <edoardott@gmail.com>
2021-05-23 21:51:52 +0200

    update

--- main.go
+++ main.go
@@ -1,3 +1,25 @@
+/*
+==========
+Cariddi
+==========
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
+
+	@Repository:  https://github.com/edoardottt/cariddi
+	@Author:      edoardottt, https://www.edoardoottavianelli.it
+*/
+
 package main
 
 import (
@@ -35,13 +57,12 @@ func main() {
 		os.Exit(0)
 	}
 
+	output.Beautify()
+
 	// ----------- TODO: check flags.dataPost --------------
 	data, _ := input.CheckDataPost(flags.DataPost)
 
 	// ----------- TODO: check ALL input -------------------
-
-	output.Beautify()
-
 	input.CheckFlags(flags)
 
 	var finalResult []string
