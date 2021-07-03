73ec61651e207f53025a18ebbdf47a74b6e98aaf
edoardottt <edoardott@gmail.com>
2021-07-03 17:17:37 +0200

    Solved Issue #3

--- scanner/secrets.go
+++ scanner/secrets.go
@@ -280,6 +280,15 @@ func GetRegexes() []Secret {
 			"",
 			"?",
 		},
+		{
+			"S3 Bucket",
+			"S3 Bucket",
+			`(?:[a-zA-Z0-9_-]+s3.amazonaws.com|[a-zA-Z0-9_.-]+amazonaws.com|` +
+				`[a-zA-Z0-9-\.\_]+\.s3\.amazonaws\.com|s3://[a-zA-Z0-9-\.\_]+|` +
+				`s3-[a-zA-Z0-9-\.\_\/]+|s3.amazonaws.com/[a-zA-Z0-9-\.\_]+)`,
+			"",
+			"?",
+		},
 	}
 	return regexes
 }
