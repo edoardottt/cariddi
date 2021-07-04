84d242ce11e10ffdbf4a21b6f6897982ae65c482
edoardottt <edoardott@gmail.com>
2021-07-04 12:56:56 +0200

    Solve #9

--- crawler/colly.go
+++ crawler/colly.go
@@ -221,10 +221,12 @@ func SecretsMatch(url string, body string, secretsFile []string) []scanner.Secre
 			if matched, err := regexp.Match(secret.Regex, []byte(body)); err == nil && matched {
 				re := regexp.MustCompile(secret.Regex)
 				match := re.FindStringSubmatch(body)
-				// avoiding false positives
-				if secret.FalsePositive != "" && !strings.Contains(match[0], secret.FalsePositive) {
-					secretFound := scanner.SecretMatched{Secret: secret, Url: url, Match: match[0]}
-					secrets = append(secrets, secretFound)
+				// Avoiding false positives
+				for _, falsePositive := range secret.FalsePositives {
+					if !strings.Contains(strings.ToLower(match[0]), falsePositive) {
+						secretFound := scanner.SecretMatched{Secret: secret, Url: url, Match: match[0]}
+						secrets = append(secrets, secretFound)
+					}
 				}
 			}
 		}
