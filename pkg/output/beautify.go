0beccfb07522f6d5946746f2a529c530ab30c606
edoardottt <edoardott@gmail.com>
2023-03-22 19:16:56 +0100

    version and banner as global constant #113

--- pkg/output/beautify.go
+++ pkg/output/beautify.go
@@ -33,20 +33,24 @@ import (
 	"github.com/fatih/color"
 )
 
+// nolint: checknoglobals
+const (
+	version = "v1.3.1-dev"
+	banner  = `                 _     _     _ _ 
+                (_)   | |   | (_)
+   ___ __ _ _ __ _  __| | __| |_ 
+  / __/ _` + "`" + ` | '__| |/ _` + "`" + ` |/ _` + "`" + ` | |
+ | (_| (_| | |  | | (_| | (_| | |
+  \___\__,_|_|  |_|\__,_|\__,_|_| `
+)
+
 // Beautify prints the banner + version.
 func Beautify() {
-	banner1 := "                 _     _     _ _ \n"
-	banner2 := "   ___ __ _ _ __(_) __| | __| (_)\n"
-	banner3 := "  / __/ _` | '__| |/ _` |/ _` | |\n"
-	banner4 := " | (_| (_| | |  | | (_| | (_| | |\n"
-	banner5 := "  \\___\\__,_|_|  |_|\\__,_|\\__,_|_| v1.3.0\n"
-	banner6 := "\n"
-	banner7 := " > github.com/edoardottt/cariddi\n"
-	banner8 := " > edoardoottavianelli.it\n"
-	banner9 := "========================================\n"
+	links := " > github.com/edoardottt/cariddi\n > edoardoottavianelli.it\n"
+	sepLine := "========================================\n"
 
-	bannerPart1 := banner1 + banner2 + banner3 + banner4 + banner5
-	bannerPart2 := banner6 + banner7 + banner8 + banner9
+	bannerPart1 := banner + version + "\n\n"
+	bannerPart2 := links + sepLine
 
 	color.Set(color.FgCyan)
 	fmt.Fprint(os.Stderr, bannerPart1)
