c1cd75deac1c4004b84583913ae4b3c26a3484a5
edoardottt <edoardott@gmail.com>
2025-04-08 10:24:59 +0200

    enhanced HTML output

--- go.mod
+++ go.mod
@@ -23,7 +23,7 @@ require (
 	github.com/nlnwa/whatwg-url v0.6.2 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
-	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/net v0.39.0 // indirect
 	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
--- pkg/output/output.go
+++ pkg/output/output.go
@@ -28,8 +28,8 @@ package output
 
 import (
 	"fmt"
+	"html"
 	"os"
-	"strings"
 
 	fileUtils "github.com/edoardottt/cariddi/internal/file"
 	"github.com/edoardottt/cariddi/pkg/input"
@@ -38,7 +38,17 @@ import (
 
 const (
 	CariddiOutputFolder = "output-cariddi"
+	ResultFile          = "results"
+	SecretsFile         = "secrets"
+	EndpointsFile       = "endpoints"
+	ExtensionsFile      = "extensions"
+	ErrorsFile          = "errors"
+	InfosFile           = "info"
 )
+
+func hasValidExtensionFlag(flag int) bool {
+	return flag >= 1 && flag <= 7
+}
 
 // PrintSimpleOutput prints line by line.
 func PrintSimpleOutput(out []string) {
@@ -62,22 +72,22 @@ func TxtOutput(flags input.Input, finalResults []string, finalSecret []scanner.S
 		fileUtils.CreateOutputFolder()
 	}
 
-	ResultFilename := fileUtils.CreateOutputFile(flags.TXTout, "results", "txt")
+	ResultFilename := fileUtils.CreateOutputFile(flags.TXTout, ResultFile, "txt")
 	for _, elem := range finalResults {
 		AppendOutputToTxt(elem, ResultFilename)
 	}
 
 	// if secrets flag enabled save also secrets
-	if flags.Secrets {
-		SecretFilename := fileUtils.CreateOutputFile(flags.TXTout, "secrets", "txt")
+	if flags.Secrets && len(finalSecret) > 0 {
+		SecretFilename := fileUtils.CreateOutputFile(flags.TXTout, SecretsFile, "txt")
 		for _, elem := range finalSecret {
 			AppendOutputToTxt(fmt.Sprintf("%s - %s in %s", elem.Secret.Name, elem.Match, elem.URL), SecretFilename)
 		}
 	}
 
 	// if endpoints flag enabled save also endpoints
-	if flags.Endpoints {
-		EndpointFilename := fileUtils.CreateOutputFile(flags.TXTout, "endpoints", "txt")
+	if flags.Endpoints && len(finalEndpoints) > 0 {
+		EndpointFilename := fileUtils.CreateOutputFile(flags.TXTout, EndpointsFile, "txt")
 
 		for _, elem := range finalEndpoints {
 			for _, parameter := range elem.Parameters {
@@ -95,24 +105,24 @@ func TxtOutput(flags input.Input, finalResults []string, finalSecret []scanner.S
 	}
 
 	// if extensions flag enabled save also secrets
-	if 1 <= flags.Extensions && flags.Extensions <= 7 {
-		ExtensionsFilename := fileUtils.CreateOutputFile(flags.TXTout, "extensions", "txt")
+	if hasValidExtensionFlag(flags.Extensions) && len(finalExtensions) > 0 {
+		ExtensionsFilename := fileUtils.CreateOutputFile(flags.TXTout, ExtensionsFile, "txt")
 		for _, elem := range finalExtensions {
 			AppendOutputToTxt(fmt.Sprintf("%s in %s", elem.Filetype.Extension, elem.URL), ExtensionsFilename)
 		}
 	}
 
 	// if errors flag enabled save also errors
-	if flags.Errors {
-		ErrorsFilename := fileUtils.CreateOutputFile(flags.TXTout, "errors", "txt")
+	if flags.Errors && len(finalErrors) > 0 {
+		ErrorsFilename := fileUtils.CreateOutputFile(flags.TXTout, ErrorsFile, "txt")
 		for _, elem := range finalErrors {
 			AppendOutputToTxt(fmt.Sprintf("%s - %s in %s", elem.Error.ErrorName, elem.Match, elem.URL), ErrorsFilename)
 		}
 	}
 
 	// if info flag enabled save also infos
-	if flags.Info {
-		InfosFilename := fileUtils.CreateOutputFile(flags.TXTout, "info", "txt")
+	if flags.Info && len(finalInfos) > 0 {
+		InfosFilename := fileUtils.CreateOutputFile(flags.TXTout, InfosFile, "txt")
 		for _, elem := range finalInfos {
 			AppendOutputToTxt(fmt.Sprintf("%s - %s in %s", elem.Info.Name, elem.Match, elem.URL), InfosFilename)
 		}
@@ -144,7 +154,7 @@ func HTMLOutput(flags input.Input, resultFilename string, finalResults []string,
 	FooterHTML(resultFilename)
 
 	// if secrets flag enabled save also secrets
-	if flags.Secrets {
+	if flags.Secrets && len(finalSecret) > 0 {
 		HeaderHTML("Secrets found", resultFilename)
 
 		for _, elem := range finalSecret {
@@ -155,7 +165,7 @@ func HTMLOutput(flags input.Input, resultFilename string, finalResults []string,
 	}
 
 	// if endpoints flag enabled save also endpoints
-	if flags.Endpoints {
+	if flags.Endpoints && len(finalEndpoints) > 0 {
 		HeaderHTML("Endpoints found", resultFilename)
 
 		for _, elem := range finalEndpoints {
@@ -176,7 +186,7 @@ func HTMLOutput(flags input.Input, resultFilename string, finalResults []string,
 	}
 
 	// if extensions flag enabled save also extensions
-	if 1 <= flags.Extensions && flags.Extensions <= 7 {
+	if hasValidExtensionFlag(flags.Extensions) && len(finalExtensions) > 0 {
 		HeaderHTML("Extensions found", resultFilename)
 
 		for _, elem := range finalExtensions {
@@ -187,7 +197,7 @@ func HTMLOutput(flags input.Input, resultFilename string, finalResults []string,
 	}
 
 	// if errors flag enabled save also errors
-	if flags.Errors {
+	if flags.Errors && len(finalErrors) > 0 {
 		HeaderHTML("Errors found", resultFilename)
 
 		for _, elem := range finalErrors {
@@ -199,13 +209,13 @@ func HTMLOutput(flags input.Input, resultFilename string, finalResults []string,
 	}
 
 	// if info flag enabled save also infos
-	if flags.Info {
+	if flags.Info && len(finalInfos) > 0 {
 		HeaderHTML("Useful informations found", resultFilename)
 
 		for _, elem := range finalInfos {
 			// Escape HTML comment to be shown on the result page
 			AppendOutputToHTML(elem.Info.Name+" - "+
-				strings.ReplaceAll(strings.ReplaceAll(elem.Match, "<", "&lt;"), ">", "&gt;")+
+				html.EscapeString(elem.Match)+
 				" in "+elem.URL, "", resultFilename, false)
 		}
 
